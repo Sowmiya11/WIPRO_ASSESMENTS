672c2a4d065391a95f3a04476476bd8a722cd7f0
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-06-08 14:39:20 +0530

    Create Addition.java

---
+++ CommandLineAss/Addition.java
@@ -0,0 +1,9 @@
+
+public class Addition {
+
+	public static void main(String[] str) {
+		int add=Integer.parseInt(str[0])+Integer.parseInt(str[1]);
+		System.out.println("the sum of "+str[0]+" and "+str[1]+" is "+add);
+	}
+
+}
