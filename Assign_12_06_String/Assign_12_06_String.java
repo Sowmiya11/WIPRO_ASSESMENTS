bd1291f8f2f66477cae0af382677de2d479f881a
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-06-08 15:05:21 +0530

    Create Assign_12_06_String.java

---
+++ Assign_12_06_String/Assign_12_06_String.java
@@ -0,0 +1,17 @@
+public class Assign_12_06_String_01 {
+
+	public static void main(String[] args) {
+		
+		String strOriginal = "ravi";
+		int len = strOriginal.length();
+		
+		String strReversed = new StringBuffer(strOriginal).reverse().toString();
+		
+		if(strOriginal.equals(strReversed))
+			System.out.println("palindrome");
+		else
+			System.out.println("not a palindrome");
+
+	}
+
+}
